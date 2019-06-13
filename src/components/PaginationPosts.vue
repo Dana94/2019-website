946141de09d79c255967ced806eee493e906a3cb
Dana Ottaviani <dana.ottaviani@gmail.com>
2019-06-13 11:30:24 -0400

    Add a project link

--- src/components/PaginationPosts.vue
+++ src/components/PaginationPosts.vue
@@ -1,17 +1,24 @@
 <template>
   <div class="flex justify-between text-xl items-center">
-    <g-link :to="previousPage" :class="{ 'text-gray-400 hover:text-gray-400 cursor-not-allowed': !showPreviousPage }">&larr; Prev</g-link>
+    <g-link
+      :to="previousPage"
+      :class="{ 'text-gray-400 hover:text-gray-400 cursor-not-allowed': !showPreviousPage }"
+    >&larr; Prev</g-link>
     <div class="text-base">Page {{ currentPage }} of {{ totalPages }}</div>
-    <g-link :to="nextPage" :class="{ 'text-gray-400 hover:text-gray-400 cursor-not-allowed': !showNextPage }">Next &rarr;</g-link>
+    <g-link
+      :to="nextPage"
+      :class="{ 'text-gray-400 hover:text-gray-400 cursor-not-allowed': !showNextPage }"
+    >Next &rarr;</g-link>
+    <font-awesome :icon="['fab', 'github']"/>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['base', 'totalPages', 'currentPage'],
+  props: ["base", "totalPages", "currentPage"],
   computed: {
     showPreviousPage() {
-      return this.currentPage !== 1
+      return this.currentPage !== 1;
     },
     previousPage() {
       return [0, 1].includes(this.currentPage - 1)
@@ -19,7 +26,7 @@ export default {
         : `${this.base}/${this.currentPage - 1}`;
     },
     showNextPage() {
-      return this.currentPage !== this.totalPages
+      return this.currentPage !== this.totalPages;
     },
     nextPage(currentPage, totalPages) {
       return this.totalPages > this.currentPage
@@ -27,5 +34,5 @@ export default {
         : `${this.base}/${this.currentPage}`;
     }
   }
-}
+};
 </script>

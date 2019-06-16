4998bb17c3cbf7dde7ff7e2f7b58367cfd3e0f6e
Dana Ottaviani <dana.ottaviani@gmail.com>
2019-06-16 17:53:08 -0400

    WIP

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -8,6 +8,13 @@
           alt="avatar"
           class="w-32 h-32 rounded-full mb-8 lg:mb-0"
         />
+        <div>I'm Dana Ottaviani, a front-end developer...</div>
+        <div>I was first intoduced to programming in my undergrad which lead me to graduating with a Computer Science degree (and Mathematics too).
+          I enjoy working in the front-end side of web development but constantly try to expand my knowledge of the back-end too.
+      
+        </div>
+        <div>I finally decided to get around having a clean, new site to showcase what I know so far and even a blog to document what I learn on my own.</div>
+        <div>Aside from programming, I enjoy learning about Blender and improving my drawing skills in traditional and digital formats.</div>
       </div>
     </div>
   </Layout>

04c4a4ff95439cff55214fc4852e5e155cc50899
Dana Ottaviani <dana.ottaviani@gmail.com>
2019-06-27 18:08:16 -0400

    Add new svgs

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -2,7 +2,9 @@
   <Layout>
     <div class="container-inner mx-auto py-16">
       <h2 class="text-4xl font-bold mb-16">Page Not Found</h2>
-      <g-image src="../../static/404.svg" />
+      <g-image src="../../static/undraw_blank_canvas.svg" />
+      <g-image src="../../static/undraw_not_found.svg" />
+
     </div>
 
   </Layout>

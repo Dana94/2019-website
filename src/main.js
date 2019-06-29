88b61baaf324bbe4b039e494f54677d3267e397e
Dana Ottaviani <dana.ottaviani@gmail.com>
2019-06-29 10:48:49 -0400

    Add new favicon

--- src/main.js
+++ src/main.js
@@ -28,7 +28,7 @@ export default function (Vue, { router, head, isClient }) {
 
   head.meta.push({
     name: 'author',
-    content: 'Andre Madarang'
+    content: 'Dana Ottaviani'
   })
 
   head.link.push({

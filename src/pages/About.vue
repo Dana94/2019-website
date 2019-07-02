56a16337de5374e351995974c359020be5bc3f6a
Dana Ottaviani <dana.ottaviani@gmail.com>
2019-07-02 11:04:29 -0400

    Center image in mobile for about me page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -10,12 +10,10 @@
         />
         <div>
           I'm Dana Ottaviani, a front-end developer. I'm currently on the look out for the next great company to work at so send an
-          <a
-            href="mailto:dana.ottaviani@gmail.com"
-          >email</a> or
-          <g-link to="/#contact">message</g-link>if you're interested.
+          <a href="mailto:dana.ottaviani@gmail.com">email</a> or <g-link to="/#contact">message</g-link> 
+          if you're interested.
         </div>
-        <br>
+        <br />
         <div>
           I went into my undergrad as undecided. I entered college with an interest
           in math and signed up for a B.A. in Mathematics. One of the requirements was an
@@ -31,21 +29,21 @@
           minor in Computer Science, but that eventually lead to a B.S. in the area since both
           Mathematics and C.S. had some class requirement overlap.
         </div>
-        <br>
+        <br />
         <div>I enjoy working in the front-end side of web development since it’s really the first thing a user sees. But I have constant curiosity in AWS, back-end, devops, I don’t want these areas to be a black box to me. I want at least a high-level understanding of these processes to expand my technical knowledge to be a more well-rounded developer.</div>
-        <br>
+        <br />
         <div>
           I finally decided to get around having a clean, new site to showcase what I know so far and even a
-          <g-link to="/blog">blog</g-link>to document what I learn on my own.
+          <g-link to="/blog">blog</g-link> to document what I learn on my own.
         </div>
-        <br>
+        <br />
         <div>Aside from programming, I enjoy learning about Blender and improving my drawing skills.</div>
-        <br>
+        <br />
         <div>I have also embraced open source and constanty keep an eye out for programs and repos I can use and contribute to.</div>
       </div>
       <div>
         <h3 class="text-2xl">Currently Using:</h3>
-        <hr>
+        <hr />
         <ul>
           <li>Operating System: Windows 10</li>
           <li>
@@ -93,9 +91,15 @@ ul {
 }
 
 img {
-  float: left;
-  margin-bottom: 1rem;
-  margin-right: 2rem;
+  margin: 0 auto;
+  margin-bottom: 2rem;
+}
+@media screen and (min-width: 425px) {
+  img {
+    float: left;
+    margin-bottom: 1rem;
+    margin-right: 2rem;
+  }
 }
 </style>
 

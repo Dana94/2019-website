faa1c51e4cbf01d54650a8c85f0e43ff26178318
Dana Ottaviani <dana.ottaviani@gmail.com>
2019-07-02 16:40:26 -0400

    Add missing alt tags

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -3,7 +3,7 @@
     <div class="container-inner mx-auto py-16">
       <h2 class="text-4xl font-bold mb-16">Page Not Found</h2>
       <p class="flex justify-center m-12 text-2xl"><g-link to="/">Back to Home</g-link></p>
-      <g-image src="../../static/undraw_blank_canvas.svg"/>
+      <g-image src="../../static/undraw_blank_canvas.svg" alt="Woman looking at a blank canvas.">
     </div>
   </Layout>
 </template>

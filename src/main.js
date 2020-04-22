a97af269aaf5cf20855b21171fa70faea5d2cb8c
Dana <dana.ottaviani@gmail.com>
2020-04-22 14:51:21 -0400

    fix: remove extra meta data

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ export default function (Vue, { router, head, isClient }) {
 
   head.meta.push({
     name: 'keywords',
-    content: 'Gridsome,Vue,Tailwind,Tailwind CSS,JavaScript,HTML,CSS,Vue.js,Vue.js'
+    content: 'Gridsome,Vue,Tailwind,Tailwind CSS,JavaScript,HTML,CSS,Vue.js'
   })
 
   head.meta.push({

68207a01339c10cb226408be217ea686703711b5
Dana <dana.ottaviani@gmail.com>
2020-07-09 18:20:47 -0400

    Add aria-label to menu button

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -22,6 +22,7 @@
             @click="toggle"
             role="button"
             type="button"
+            aria-label="Menu"
             class="flex items-center px-3 py-2 border rounded border-gray-500 hover:text-gray-600 hover:border-gray-600"
           >
             <svg

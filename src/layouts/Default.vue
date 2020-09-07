ac3c28eb86cfba64ab82afde8155c8ca3301b0c2
Dana <dana.ottaviani@gmail.com>
2020-09-07 10:24:28 -0400

    fix: add noreferrer in rel attribute
    finish

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -86,15 +86,15 @@
           <div>Copyright 2019. All rights reserved.</div>
           <div>
             Footer icons (first 5) by
-            <a href="https://icons8.com/" target="_blank" rel="noopener" class="hover:text-purple-300">icons8</a>.
+            <a href="https://icons8.com/" target="_blank" rel="noopener noreferrer" class="hover:text-purple-300">icons8</a>.
           </div>
           <div>
-            <a href="https://github.com/Dana94/website" target="_blank" rel="noopener" class="hover:text-purple-300">View Source</a>
+            <a href="https://github.com/Dana94/website" target="_blank" rel="noopener noreferrer" class="hover:text-purple-300">View Source</a>
           </div>
         </div>
         <ul class="social-icons">
           <li class="flex justify-center items-center">
-            <a href="https://docs.google.com/document/d/17YrN4uSYJdU_Lk0B05p2MS1rBNsYg6wTS8HfFFk_8fc/edit?usp=sharing" target="_blank" rel="noopener" aria-label="resume" class="text-white hover:text-gray-400">
+            <a href="https://docs.google.com/document/d/17YrN4uSYJdU_Lk0B05p2MS1rBNsYg6wTS8HfFFk_8fc/edit?usp=sharing" target="_blank" rel="noopener noreferrer" aria-label="resume" class="text-white hover:text-gray-400">
               <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="40" height="40" viewBox="0 0 172 172" style=" fill:#000000;"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal">
                 <path d="M0,172v-172h172v172z" fill="none">
                 </path>
@@ -120,7 +120,7 @@
             <a
               href="https://www.linkedin.com/in/danaottaviani/"
               target="_blank"
-              rel="noopener"
+              rel="noopener noreferrer"
               aria-label="LinkedIn profile"
               class="text-white hover:text-gray-400"
             >
@@ -134,7 +134,7 @@
             <a
               href="https://github.com/Dana94"
               target="_blank"
-              rel="noopener"
+              rel="noopener noreferrer"
               aria-label="GitHub profile"
               class="text-white hover:text-gray-400"
             >
@@ -148,7 +148,7 @@
             <a
               href="https://gitlab.com/Dana94"
               target="_blank"
-              rel="noopener"
+              rel="noopener noreferrer"
               aria-label="GitLab profile"
               class="text-white hover:text-gray-400"
             >
@@ -159,7 +159,7 @@
             </a>
           </li>
           <li class="flex justify-center items-center">
-            <a href="https://dev.to/dana94" target="_blank" rel="noopener" aria-label="DEV profile" class="text-white hover:text-gray-400">
+            <a href="https://dev.to/dana94" target="_blank" rel="noopener noreferrer" aria-label="DEV profile" class="text-white hover:text-gray-400">
               <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
               width="30" height="30" viewBox="0 0 448 512"><path fill="#ffffff" d="M120.12 208.29c-3.88-2.9-7.77-4.35-11.65-4.35H91.03v104.47h17.45c3.88 0 7.77-1.45 11.65-4.35 3.88-2.9 5.82-7.25 5.82-13.06v-69.65c-.01-5.8-1.96-10.16-5.83-13.06zM404.1 32H43.9C19.7 32 .06 51.59 0 75.8v360.4C.06 460.41 19.7 480 43.9 480h360.2c24.21 0 43.84-19.59 43.9-43.8V75.8c-.06-24.21-19.7-43.8-43.9-43.8zM154.2 291.19c0 18.81-11.61 47.31-48.36 47.25h-46.4V172.98h47.38c35.44 0 47.36 28.46 47.37 47.28l.01 70.93zm100.68-88.66H201.6v38.42h32.57v29.57H201.6v38.41h53.29v29.57h-62.18c-11.16.29-20.44-8.53-20.72-19.69V193.7c-.27-11.15 8.56-20.41 19.71-20.69h63.19l-.01 29.52zm103.64 115.29c-13.2 30.75-36.85 24.63-47.44 0l-38.53-144.8h32.57l29.71 113.72 29.57-113.72h32.58l-38.46 144.8z" style="&#10;    /* fill: white; */&#10;"/></svg>
             </a>
